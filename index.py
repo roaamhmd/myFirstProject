895e01a5eaebedc96bce3d599f71652c2ba7a0d8
Aarreejj02 <abukishkareej2@gmail.com>
2026-08-18 11:17:08 +0300

    changed

--- index.py
+++ index.py
@@ -17,9 +17,14 @@ def main():
             table_data = []
 
             for row in rows:
+
                 cells = [col.inner_text().strip() for col in row.locator("th, td").all()]
-                if cells:
-                    table_data.append(cells)
+    
+                filtered_cells = [c for c in cells if c != ""]
+    
+                if filtered_cells:
+                    table_data.append(filtered_cells)
+                
 
             if len(table_data) > 1:
                 df = pd.DataFrame(table_data[1:], columns=table_data[0])
